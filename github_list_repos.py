c924add5408295d2e4cea451fedd4eaa4ddaac4d
Jaime H Rozo <jhrozo@yahoo.com>
2019-06-26 22:51:55 -0300

    Update content of variable api_url_base
    The content of the variable api_url_base is incomplete and is causing HTTP 404.
    
    According to GitHub doc https://developer.github.com/v3/repos/#list-user-repositories, in order to GET the user repositories, the base URL must contain the path element "/users".  So, the updated variable must read: 'https://api.github.com/users/'.

--- github_list_repos.py
+++ github_list_repos.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-api_url_base = 'https://api.github.com/'
+api_url_base = 'https://api.github.com/users/'
 headers = {'Content-Type': 'application/json',
            'User-Agent': 'Python Student',
            'Accept': 'application/vnd.github.v3+json'}
